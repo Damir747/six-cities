5455fc759ebdfda44f98f40208ee9b95c6f86d4f
Damir <102433257+Damir747@users.noreply.github.com>
2023-07-22 17:12:20 +0300

    try to start

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
-const bodyElement = document.querySelector(`body`);
-bodyElement.textContent = `Hello, Damir!`;
+import React from "react";
+import ReactDom from "react-dom";
+
+import App from "./components/app/App";
+import { OfferCards } from "./mock-data";
+
+ReactDom.render(
+  <App offersCards={OfferCards} />,
+  document.getElementById(`root`),
+);
