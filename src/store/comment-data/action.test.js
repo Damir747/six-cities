8f1cc23a6148d8ccbc862915e0955ca5f3f71d32
Damir <102433257+Damir747@users.noreply.github.com>
2024-01-01 22:38:12 +0300

    jsdoc for utils; 1st test for actions

---
+++ src/store/comment-data/action.test.js
@@ -0,0 +1,22 @@
+import { commentPost } from "./actions";
+import { COMMENT_POST } from "./actions-types";
+
+describe(`Actions in comments work correctly`, () => {
+  it(`Actions for new 50-letters comment work correctly`, () => {
+    const expectedAction = {
+      type: COMMENT_POST,
+      payload: '12345678901234567890123456789012345678901234567890',
+    };
+    const textComment = '12345678901234567890123456789012345678901234567890';
+    expect(commentPost(textComment)).toEqual(expectedAction);
+  });
+
+  it(`Actions for new 152-letters comment work correctly`, () => {
+    const expectedAction = {
+      type: COMMENT_POST,
+      payload: '12345678901234567890123456789012345678901234567890 12345678901234567890123456789012345678901234567890 12345678901234567890123456789012345678901234567890',
+    };
+    const textComment = '12345678901234567890123456789012345678901234567890 12345678901234567890123456789012345678901234567890 12345678901234567890123456789012345678901234567890';
+    expect(commentPost(textComment)).toEqual(expectedAction);
+  });
+});
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,5 +1,10 @@
 import { BOOKMARKS, LevelFrame } from '../const';
 
+/**
+ * Capitalize first letter in string
+ * @param {string} str
+ * @return {string}
+ */
 const capitalizeFirstLetter = (str) => {
   if (!str) {
     return '';
@@ -7,6 +12,11 @@ const capitalizeFirstLetter = (str) => {
   return str[0].toUpperCase() + str.slice(1);
 };
 
+/**
+ * Convert rating to 100%-scale
+ * @param {number} rating
+ * @return {number}
+ */
 const roundRating = (rating) => {
   if (rating <= 5) {
     return Math.round(rating) * 20;
@@ -14,17 +24,40 @@ const roundRating = (rating) => {
   return Math.round(rating / 20) * 20;
 };
 
+/**
+ * Convert rating from 100-scale to 5-scale
+ * using in Property
+ * @param {number} rating
+ * @return {number}
+ */
 const numberRating = (rating) => Math.round(rating / 20).toFixed(1);
 
+/**
+ * Join classnames from array to string
+ * @param {Array} arr
+ * @return {string}
+ */
 const classname = (...arr) => arr.join(' ');
 
-// className для кнопки bookmark в зависимости от расположения кнопки (frame) и значения
+/**
+ * Join classnames for bookmark-button for Forms (frame)
+ * className для кнопки bookmark в зависимости от расположения кнопки (frame) и значения
+ * @param {string} frame
+ * @param {string} bookmark
+ * @return {string}
+ */
 const bookmarkClassname = (frame, bookmark) => {
   return classname(`${frame}__bookmark-button`, `button`,
     bookmark === BOOKMARKS.IN ? `${frame}__bookmark-button--active` : '');
 };
 
-// className для Room в зависимости от расположения компонента (frame)
+/**
+ * Join classnames for room-card for using Forms (frame)
+ * className для Room в зависимости от расположения компонента (frame)
+ * using in Room
+ * @param {string} frame
+ * @return {string}
+ */
 const roomClassname = (frame) => {
   if (frame === LevelFrame.NEAR_PLACES) {
     return classname(`${frame}__card`, 'place-card');
@@ -32,7 +65,13 @@ const roomClassname = (frame) => {
   return classname(`${frame}__place-card`, 'place-card');
 };
 
-// className для картинки в Room в зависимости от расположения компонента (frame)
+/**
+ * className for image in Room for using Form (frame)
+ * className для картинки в Room в зависимости от расположения компонента (frame)
+ * using in Room
+ * @param {string} frame
+ * @return {string}
+ */
 const roomImageClassname = (frame) => classname(`${frame}__image-wrapper`, 'place-card__image-wrapper');
 
 export { capitalizeFirstLetter, roundRating, numberRating, classname, bookmarkClassname, roomClassname, roomImageClassname };
