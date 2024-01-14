dc6edccccddf8d72314e7662f0ef3b25dab4cdfe
Damir <102433257+Damir747@users.noreply.github.com>
2024-01-14 23:22:09 +0300

    test: cityMap

--- src/components/city-map/city-map.test.js
+++ src/components/city-map/city-map.test.js
@@ -9,4 +9,13 @@ describe(`Test HOC CityMap`, () => {
     render(CityMap);
     expect(screen.getByText(/MockCityMap/i)).toBeInTheDocument();
   });
+  it(`Check props count`, () => {
+    const cityMap = jest.fn();
+    render(cityMap({ rooms: [42] }));
+    expect(cityMap).toHaveBeenCalledWith(
+      expect.objectContaining({
+        rooms: expect.any(Array)
+      }),
+    );
+  });
 });
